33b675c088cfd222be56848f67725da9e487b38d
Indunil Udayangana <indunilud@codegen.net>
2024-04-19 00:39:35 +0530

    update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
-module.exports={
-    MONGOURI:process.env.MOGOURI,
-    JWT_SECRET:process.env.JWT_SEC,
-    SENDGRID_API:process.env.SENDGRID_API,
-    EMAIL:process.env.EMAIL
-}
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://udayangana98:NqAi5CdU69UZCfoK@cluster0.dd0piq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  JWT_SECRET: "sxhsscuuygxastr",
+  SENDGRID_API: "huytiufgyfd56ertrytf",
+  EMAIL: "localhost:3000",
+};
